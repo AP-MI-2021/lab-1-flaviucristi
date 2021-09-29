163a6cb2b43c488e8aa9d128ede242df50822a61
Muresan Flaviu <flaviucristi@yahoo.com>
2021-09-29 20:07:36 +0300

    Al doilea comit

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ Returneaza produsul numerelor din lista lst.
 def get_product(lst):
   p=1
   for i in lst:
-    p=p*lst[i]
+    p=p*i
+  return p
 
   
   
@@ -28,8 +29,8 @@ def get_product(lst):
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
-  d=int(x)
-  i=int(y)
+  d=x
+  i=y
 
   while i>0:
     r=d%i
@@ -53,7 +54,20 @@ def get_cmmdc_v2(x, y):
   
   
 def main():
-  # interfata de tip consola aici
+while True:
+  print("1. Determinati daca numarul este prim")
+  print("2. Calculeaza produsul a n numere")
+  print("3. Determina cel mai mare divizor comun prin prima metoda")
+  print("4. Determina cel mai mare divizor comun prin a doua metoda")
+  print("x. Iesire")
+
+  optiune= input("Dati o optiune: ")
+
+  if optiune == "1":
+    n=int(input("Dati numar"))
+    print(is_prime(n))
+  elif optiune == "2":
+
 
 if __name__ == '__main__':
   main()
